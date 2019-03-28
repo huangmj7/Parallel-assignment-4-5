306b6dcfdb5aceae2e04ed5f06e160bcafe9b631
huangmj7 <huangm7@rpi.edu>
2019-03-27 23:24:46 -0400

    Now

--- CellWorld.c
+++ CellWorld.c
@@ -33,7 +33,6 @@
 #define ALIVE 1
 #define DEAD  0
 #define rows 32
-#define threshold 0.25
 
 /***************************************************************************/
 /* Global Vars *************************************************************/
@@ -52,7 +51,9 @@ int **universe; //universe for each rank
 int *ghost_up;      //the ghost row for top row
 int *ghost_down;    //the ghost row for buttom row
 
-
+double threshold = 0.25;
+int mpi_myrank = -1;
+int mpi_commsize = -1;
 /***************************************************************************/
 /* Function Decs ***********************************************************/
 /***************************************************************************/
@@ -60,12 +61,12 @@ int *ghost_down;    //the ghost row for buttom row
 // You define these
 
 
-int generate(int row_index,int key)
+int generate(int row_index,int num_row,int key)
 {
 	//int key = row_index+mpi_myrank*(rows/mpi_commsize)
 	
 	//get RNG
-	float rng =  GenVal(key);
+	double rng =  GenVal(key);
         //printf("%d\n == %lf",row_index,rng);
 	
 	//get state
@@ -78,14 +79,14 @@ int generate(int row_index,int key)
         
         if(rng < threshold){
 
-                for(int i=0; i<rows; i++){
-                        float random = (float)rand()/RAND_MAX;
+                for(int col=0; col<rows; col++){
+                        double random = (double)rand()/RAND_MAX;
                         //printf("[%f]\n",random);
                         if(random > 0.5){
-                                universe[row_index][i] = 1;
+                                universe[row_index][col] = 1;
                                 lives ++;
                         }
-                        else{universe[row_index][i] = 0;}
+                        else{universe[row_index][col] = 0;}
                 }
 
                 return lives;
@@ -93,12 +94,11 @@ int generate(int row_index,int key)
 
 	//rule
 	
-	for(int i=0; i<rows; i++){
+	for(int col=0; col<rows; col++){
 		
-  		//printf("[%d] ",universe[row_index*rows+i]);
-                l = i-1;
+                l = col-1;
 		if(l < 0){l = rows-1;}
-		r = i+1;
+		r = col+1;
 		if(r>= rows){r = 0;}
 
                 left = universe[row_index][l];
@@ -107,12 +107,12 @@ int generate(int row_index,int key)
 		
 		if(row_index == 0){
 
-			up = ghost_up[i];
+			up = ghost_up[col];
 			upleft = ghost_up[l];
 			upright = ghost_up[r];
 		}
 		else{
-			up = universe[row_index-1][i];
+			up = universe[row_index-1][col];
                         upleft = universe[row_index-1][l];
                         upright = universe[row_index-1][r];
 
@@ -120,29 +120,25 @@ int generate(int row_index,int key)
 
 		
 
-		if(row_index == rows-1){
+		if(row_index == num_row-1){
 
-                        down = ghost_down[i];
+                        down = ghost_down[col];
                         downleft = ghost_down[l];
                         downright = ghost_down[r];
                 }
                 else{
-                        down = universe[row_index+1][i];
+                        down = universe[row_index+1][col];
                         downleft = universe[row_index+1][l];
                         downright = universe[row_index+1][r];
 
                 }
 
           
-                int state = up + down + left + right + upright + upleft + downright +  downleft;
-		//printf("[%d][%d] \n",row_index,universe[row_index][i]);
-                //universe[row_index][i] = 0;
-                //printf("[%d][%d] \n",row_index,universe[row_index][i]);
-
+                int state = up + down + left + right + upright + upleft + downright +  downleft;	
 		
-                if(universe[row_index][i] == 1){                                                                 //<---------------- Error occure here
+                if(universe[row_index][col] == 1){                                                                 
 
-                       if(state < 2 || state > 3){universe[row_index][i] = 0;} //underpopulate & overpopulate
+                       if(state < 2 || state > 3){universe[row_index][col] = 0;} //underpopulate & overpopulate
                        else{lives ++;}
 		       
                 }
@@ -151,7 +147,7 @@ int generate(int row_index,int key)
                 
                 else{
                         if(state == 3){
-                                universe[row_index][i] = 1;
+                                universe[row_index][col] = 1;
                                 lives ++;
                         } //reporduction
                 }
@@ -173,10 +169,8 @@ int communicate(int *ghost_up, int *ghost_down,MPI_Comm comm)   //communication 
 {
 	
 	/*get mpi info*/
-	int mpi_commsize = -1;
-        int mpi_myrank = -1;
-        MPI_Comm_size(comm,&mpi_commsize);
-        MPI_Comm_rank(comm,&mpi_myrank);
+        //MPI_Comm_size(comm,&mpi_commsize);
+        //MPI_Comm_rank(comm,&mpi_myrank);
         if(mpi_commsize == -1|| mpi_myrank == -1){
                 perror("ERROR: MPI failed\n");
                 return -1;
@@ -185,7 +179,7 @@ int communicate(int *ghost_up, int *ghost_down,MPI_Comm comm)   //communication 
 	/*calculate address*/
 
 	 MPI_Request send_require,recv_require;
-	 MPI_Status send_status, recv_status;
+	 MPI_Status recv_status;
 	 int up_address =  mpi_myrank -1; //the address for send the top row and receive the ghost_up layer
 	 int down_address = mpi_myrank +1;  //the address for send the buttom row and receive the ghost_down layer
 	 
@@ -212,11 +206,11 @@ int communicate(int *ghost_up, int *ghost_down,MPI_Comm comm)   //communication 
 
     	//Send to up row
     	MPI_Isend(ghost_up,rows,MPI_INT,up_address,1,MPI_COMM_WORLD,&send_require);
-    	MPI_Wait(&send_require,&send_status);
+    	MPI_Barrier(comm);
 	
 	//send to down row
 	MPI_Isend(ghost_down,rows,MPI_INT,down_address,1,MPI_COMM_WORLD,&send_require);
-	MPI_Wait(&send_require,&send_status);
+	MPI_Barrier(comm);
 
 	/*Receive*/
 
@@ -243,9 +237,7 @@ int communicate(int *ghost_up, int *ghost_down,MPI_Comm comm)   //communication 
 int main(int argc, char *argv[])
 {
 //    int i = 0;
-    int mpi_myrank;
-    int mpi_commsize;
-    time_t t;
+      time_t t;
 // Example MPI startup and using CLCG4 RNG
     MPI_Init( &argc, &argv);
     MPI_Comm_size( MPI_COMM_WORLD, &mpi_commsize);
@@ -297,7 +289,7 @@ int main(int argc, char *argv[])
             return -1;
     }
    
-    int tick = 1;
+    int tick = 10;
     for(int i=0; i<num_row; i++){
                     for(int j=0;j<rows; j++){universe[i][j] = 1;}
             }
@@ -313,20 +305,18 @@ int main(int argc, char *argv[])
            //printf("[%d] [%s] [%d]\n",mpi_myrank,"u",ghost_up[1]);
            //printf("[%d] [%s] [%d]\n",mpi_myrank,"d",ghost_down[1]);
 	   
-	   for(int i=7; i<8; i++){generate(i,(i+mpi_myrank*num_row));}
+	   for(int i=0; i<num_row; i++){generate(i,num_row,(i+mpi_myrank*num_row));}
     }
 
     
-    //printf("--------------------------------------------------------------\n");
-    //for(int i=0; i<num_row; i++){
-            //printf("[%d] ",i+mpi_myrank*num_row);
-	    //for(int j=0; j<rows; j++){
-		   //printf("%d ",universe[i][j]);
-	    //}
-	    //printf("\n");
-    //}
-    //printf("--------------------------------------------------------------\n");
-    //
+    for(int i=0; i<num_row; i++){
+            printf("[%d] ",i+mpi_myrank*num_row);
+	    for(int j=0; j<rows; j++){
+		   printf("%d ",universe[i][j]);
+	    }
+	    printf("\n");
+    }
+   
 
     for(int i=0; i<num_row; i++){free(universe[i]);}
     free(universe);
